d491952c61ee4130367ccff6a89030d4f8d58733
Graham Goudeau <grahamgoudeau@gmail.com>
2016-01-18 01:29:37 -0600

    Added another resource to track

--- test.c
+++ test.c
@@ -3,6 +3,7 @@
 
 // sudo apt-get install libncurses5-dev
 // http://stackoverflow.com/questions/4025891/create-a-function-to-check-for-key-press-in-unix-using-ncurses
+// https://viget.com/extend/game-programming-in-c-with-the-ncurses-library
 int main() {
     initscr();
     noecho();
